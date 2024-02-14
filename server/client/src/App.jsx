b2e7c91094d4d72e4e538f83b8c78bf4393254bb
Yash Sharma <yashsharma2493@gmail.com>
2024-02-14 22:40:14 +0530

    fixes

--- server/client/src/App.jsx
+++ server/client/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
   const [dummyData, setDummyData] = React.useState([])
   const [jsonData, setJsonData] = React.useState({})
   const [summary, setSummary] = React.useState("")
-  const [loading, setLoading] = React.useState(true)
+  const [loading, setLoading] = React.useState(false)
 
   const getPlatforms = React.useRef(() => { })
 
@@ -24,17 +24,17 @@ export default function App() {
 
   async function compare() {
     try {
-      if(jsonData.corePlatform === undefined || jsonData.targetPlatform === undefined){
+      if (jsonData.corePlatform === undefined || jsonData.targetPlatform === undefined) {
         alert('Please select both platforms')
         return
       }
 
-      if(jsonData.corePlatform.platformName === jsonData.targetPlatform){
+      if (jsonData.corePlatform.platformName === jsonData.targetPlatform) {
         alert('Please select different platforms')
         return
       }
 
-      if(jsonData.corePlatform.features.length === 0 ){
+      if (jsonData.corePlatform.features.length === 0) {
         alert('Please select features for core platform')
         return
       }
@@ -63,33 +63,38 @@ export default function App() {
     <>
       <div className='bg-[#D4D3DD] w-full m-auto py-12 '>
         <div className='flex justify-around items-start w-11/12 md:w-10/12 mx-auto flex-col md:flex-row'>
-          <div className='p-10 w-full md:w-[40%] border rounded-2xl bg-gray-100'>
+          <div className='p-10 my-2 w-full md:w-[40%] border rounded-2xl bg-gray-100'>
             {
               dummyData.length > 0 &&
               <CompanySelector companyData={dummyData} setData={setJsonData} data={jsonData} />
             }
           </div>
-          <div className='flex flex-col justify-center items-center'>
-            <button className="btn btn-primary" onClick={compare}>Learn</button>
-            <dialog id="my_modal_3" className="modal">
-              <div className="modal-box bg-white border-4 border-green-500 text-black mx-h-[60vh] overflow-y-scroll scroll-hide">
-                <form method="dialog">
-                  <button className="btn btn-sm btn-circle border-black btn-ghost absolute right-2 top-2">✕</button>
-                </form>
-                <h3 className="font-bold text-lg">Your Result</h3>
-                <div className="py-4 ">
-                  {summary.split('\n').map((line, index) => (
-                    <p key={index} className='my-2'>{line}</p>
-                  ))}
+          <div className='w-full md:w-[40%]'>
+            <div className='p-10 my-2 border rounded-2xl bg-gray-100'>
+              {
+                dummyData.length > 0 && (
+                  <>
+                    <CompanySelector2 companyData={dummyData} setData={setJsonData} />
+                  </>
+                )
+              }
+            </div>
+            <div className='text-end'>
+              <button className="btn btn-primary my-3" onClick={compare}>Learn</button>
+              <dialog id="my_modal_3" className="modal text-start">
+                <div className="modal-box bg-white border-4 border-green-500 text-black mx-h-[60vh] overflow-y-scroll scroll-hide">
+                  <form method="dialog">
+                    <button className="btn btn-sm btn-circle border-black btn-ghost absolute right-2 top-2">✕</button>
+                  </form>
+                  <h3 className="font-bold text-lg">Your Result</h3>
+                  <div className="py-4 ">
+                    {summary.split('\n').map((line, index) => (
+                      <p key={index} className='my-2'>{line}</p>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            </dialog>
-          </div>
-          <div className='p-10 w-full md:w-[40%] border rounded-2xl bg-gray-100'>
-            {
-              dummyData.length > 0 &&
-              <CompanySelector2 companyData={dummyData} setData={setJsonData} />
-            }
+              </dialog>
+            </div>
           </div>
         </div>
       </div>
